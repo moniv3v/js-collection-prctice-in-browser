418233b4654006f5b85e6da61592900190733d86
Monique <moniquechan1220@gmail.com>
2018-11-12 17:50:36 +0800

    Update section3

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,32 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
+  var collectionC = [];
+
+  for (let a of collectionA) {
+    var exist = false;
+    for (let c of collectionC) {
+      if (c.key == a) {
+        c.count = c.count + 1;
+        exist = true;
+      }
+    }
+    if (!exist) {
+      if(a.length>1){        
+        var str = a.split('-');
+        collectionC.push({ "key": str[0], "count": parseInt(str[1]) });
+      }else{
+        collectionC.push({ "key": a, "count": 1 });
+      }
+      
+    }
+  }
+  for (let b of objectB['value']) {
+    for (let c of collectionC) {
+      if (c.key == b) {
+        c.count -= parseInt(c.count / 3);
+      }
+    }
+  }
+  return collectionC;
 }
