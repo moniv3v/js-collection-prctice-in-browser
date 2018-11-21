3a9913b6eebf1f2c586533d68cedf267f31b0f8f
Monique <moniquechan1220@gmail.com>
2018-11-21 11:42:52 +0800

    use filter in practice-1

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,15 +1,5 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  const sameElements = [];
-  var count=0;
-  for(var i=0;i<collectionA.length;i++){
-    for(var j=0;j<collectionB.length;j++){
-      if(collectionA[i]==collectionB[j]){
-        sameElements[count] = collectionA[i];
-        count++
-      }
-    }
-  }
-  return sameElements;
+  return collectionA.filter(a => collectionB.includes(a));
 }
