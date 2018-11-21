d214a475377352d937120e8df8e83322c8b008e9
Monique <moniquechan1220@gmail.com>
2018-11-21 11:48:12 +0800

    Use .map and .filter in practice-4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,15 +1,6 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-  const sameElements = [];
-  var count=0;
-  for(var i=0;i<collectionA.length;i++){
-    for(var j=0;j<objectB['value'].length;j++){
-      if(collectionA[i]['key']==objectB['value'][j]){
-        sameElements[count] = collectionA[i]['key'];
-        count++
-      }
-    }
-  }
-  return sameElements;
+function collectSameElements(collectionA, collectionB) {
+  return collectionA.map(a=>a.key).filter(a => collectionB.value.includes(a));
 }
+
